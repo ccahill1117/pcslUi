dac4051f6144d715bb79cfe37543fad7b42855a6
Christopher Cahill <christopher.cahill@evanta.com>
2021-04-07 21:28:35 -0700

    login update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,11 +3,29 @@
     <h1>This is a login page</h1>
     <v-text-field
       label="Username"
+      v-model="input.username"
       clearable>
     </v-text-field>
     <v-text-field
       label="Password"
+      v-model="input.password"
       clearable>
     </v-text-field>
+    <p>down here we show these things</p>
+    <p>{{input.username}}</p>
+    <p>{{input.password}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Login',
+
+  data: () => ({
+    input: {
+      username: "",
+      password: ""
+    }
+  }),
+};
+</script>
